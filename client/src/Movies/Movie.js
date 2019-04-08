d0ca976d2e6d24a8a01f449ab262a16dfaa26d36
Eli Citron <ebcitron@gmail.com>
2019-04-08 15:32:40 -0500

    Working

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,7 +11,7 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.state.movie.match.params.id === `${this.state.movie.id}`;
     this.fetchMovie(id);
   }
 
